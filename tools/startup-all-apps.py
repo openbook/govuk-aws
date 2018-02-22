ad70b3d6431cfb64628631298abe81d569e756a0
Sebastian Schmieschek <sebastian@schmie.net>
2018-02-22 14:04:51 +0000

    now with added none-printing, but actual execution

--- tools/startup-all-apps.py
+++ tools/startup-all-apps.py
@@ -48,7 +48,7 @@ app_count = app_list.__len__()
 
 #pool = thread_pool(app_count)
 pool = thread_pool(4)
-results = pool.map(print,app_list)
+results = pool.map(subprocess.run,app_list)
 
 pool.close()
 pool.join()
